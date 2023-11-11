b22b8311e243aecbcc8b6e1fa2fc2c4c9f592d62
caldotdev <cluckenbach@protonmail.com>
2023-11-11 13:02:19 +0100

    feat: :sparkles: add conditional editor command
    this is only available for PDFs

--- main.ts
+++ main.ts
@@ -1,12 +1,4 @@
-import {
-	App,
-	Editor,
-	MarkdownView,
-	Modal,
-	Plugin,
-	PluginSettingTab,
-	Setting,
-} from "obsidian";
+import { App, Notice, Plugin, PluginSettingTab, Setting } from "obsidian";
 
 import { setHandshakeStatus } from "utils";
 
@@ -21,6 +13,7 @@ const DEFAULT_SETTINGS: CyrusSettings = {
 export default class Cyrus extends Plugin {
 	settings: CyrusSettings;
 	status: HTMLElement;
+	canProcess = false;
 
 	async onload() {
 		await this.loadSettings();
@@ -29,36 +22,17 @@ export default class Cyrus extends Plugin {
 
 		setHandshakeStatus(this.settings.serverUrl, this.status);
 
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, ctx: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
+			id: "cyrus-process-pdf",
+			name: "Process PDF",
 			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
+				const pdfView =
+					this.app.workspace.getActiveFile()?.extension === "pdf";
+				if (pdfView) {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						new Notice("Processing PDF");
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -67,7 +41,6 @@ export default class Cyrus extends Plugin {
 			},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new CyrusSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
@@ -94,22 +67,6 @@ export default class Cyrus extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText("Woah!");
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
 
